1bc4b8732e3366c4fb8ad88a818c1af6e7603a72
Soha Ahmed <94359655+soha100@users.noreply.github.com>
2024-03-06 16:41:34 +0200

    App5

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 let btn = document.querySelector(".btn");
 let div = document.querySelector(".mydiv");
-
+let button = document.querySelector(".button");
+let body = document.querySelector("body");
+let BackgrounColors = ["black", "green", "white", "blue", "red"];
 let Colors = ["red", "green", "blue", "white", "black"];
 let i = 0;
-
+let x = 0;
 btn.onclick = function () {
   div.style.backgroundColor = Colors[i];
   i++;
   if (i == 5) {
     i = 0;
+  }
+};
+button.onclick = function () {
+  body.style.backgroundColor = BackgrounColors[x];
+  x++;
+  if (x == 5) {
+    x = 0;
   }
 };
